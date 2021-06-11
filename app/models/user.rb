e77e09ef82de2177b84fadca63f5b3f9c7e219ef
mstk7342 <m7s3t4k228@gmail.com>
2021-06-12 01:05:24 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   validates :nickname, presence: true, length: { maximum: 6 }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
 
+  validates :nickname, presence: true, length: { maximum: 6 }
+   
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Password Include both letters and numbers'
 end
